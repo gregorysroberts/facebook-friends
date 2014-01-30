494f4467b122b232edf66062be87d5ca0c2ae03a
Greg Roberts <gregorysroberts@gmail.com>
2014-01-29 20:57:55 -0700

    Adds sort function

--- FriendController.js
+++ FriendController.js
@@ -2,6 +2,11 @@ app.controller('FriendController', function($scope, $http){
    	$http.get('https://s3.amazonaws.com/intuiplan_company_files/production/files/public/FriendData.json')
    		.success(function(data){
         	$scope.friends = data.results;
+        	angular.forEach($scope.friends, function(friend){
+        		friend.friend_count = parseInt(friend.friend_count, 10);
+        	});
     });
+
+   	var attribute 
 
 });
