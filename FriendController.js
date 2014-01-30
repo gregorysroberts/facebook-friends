7735d2ad14f448d96b7d429f57f269dd414c239c
Greg Roberts <gregorysroberts@gmail.com>
2014-01-29 19:57:18 -0700

    Adds filter options

--- FriendController.js
+++ FriendController.js
@@ -4,9 +4,4 @@ app.controller('FriendController', function($scope, $http){
         	$scope.friends = data.results;
     });
 
-    $scope.name = 'Cali Fornia';
-    $scope.current_location = 'New Port Beach, California, United States';
-    $scope.status = 'I hate the snow. I wish I was on the beach right now!!!';
-    $scope.friend_count = '1,367';
-    $scope.pic_square = 'https://scontent-a.xx.fbcdn.net/hprofile-ash3/833_10200838803828124_1233160321_a.jpg';
 });
