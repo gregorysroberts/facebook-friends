b880fe376191689cd2c0568c77c7c555b2e3e612
Greg Roberts <gregorysroberts@gmail.com>
2014-01-29 19:20:46 -0700

    Adds list of friends

--- FriendController.js
+++ FriendController.js
@@ -1,10 +1,12 @@
-app.controller('FriendController', function($scope){
-    $.get('https://s3.amazonaws.com/intuiplan_company_files/production/files/public/FriendData.json', function(){
-        console.log('got the data');
-    })
+app.controller('FriendController', function($scope, $http){
+   	$http.get('https://s3.amazonaws.com/intuiplan_company_files/production/files/public/FriendData.json')
+   		.success(function(data){
+        	$scope.friends = data.results;
+    });
+
     $scope.name = 'Cali Fornia';
-    $scope.location = 'New Port Beach, California, United States';
+    $scope.current_location = 'New Port Beach, California, United States';
     $scope.status = 'I hate the snow. I wish I was on the beach right now!!!';
-    $scope.friendCount = '1,367';
-    $scope.pictureURL = 'https://scontent-a.xx.fbcdn.net/hprofile-ash3/833_10200838803828124_1233160321_a.jpg';
+    $scope.friend_count = '1,367';
+    $scope.pic_square = 'https://scontent-a.xx.fbcdn.net/hprofile-ash3/833_10200838803828124_1233160321_a.jpg';
 });
